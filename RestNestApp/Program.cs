fe3e4e1e6b25edc0acbff92ff430aa379710bbed
MMdx <mirea.cloud@inbox.ru>
2020-06-15 07:33:03 +0300

    Initial commit

---
+++ RestNestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestNestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample booking app v.1.0.0");
+            Console.ReadKey(true);
+        }
+    }
+}
